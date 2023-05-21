d69e1bd1546ff88ba7eabd99b3f367cfe875e5cd
unknown <User@ngdu-2033.ngdu.beloil.by>
2023-05-21 13:00:19 +0300

    'SPR'

---
+++ src/spravochniki/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from . import models
+admin.site.register(models.Author)
+admin.site.register(models.Izdatelstvo)
+admin.site.register(models.Zhanr)
---
+++ src/spravochniki/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(
+        verbose_name='Name author',
+        max_length=50
+    )
+    description = models.TextField(
+        verbose_name='Description author',
+        null =True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+class Izdatelstvo(models.Model):
+    name = models.CharField(
+        verbose_name='Name izdatelstvo',
+        max_length=50
+    )
+    description = models.TextField(
+        verbose_name='Description izdatelstvo',
+        null =True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+ 
+class Zhanr(models.Model):
+    name = models.CharField(
+        verbose_name='Name zhanr',
+        max_length=50
+    )
+    description = models.TextField(
+        verbose_name='Description zhanr',
+        null =True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+ 
